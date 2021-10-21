b78f8dd8d55cb0c2ad8e1811866fc7d920ce1e52
LiYuanits <luo_book@163.com>
2021-10-21 16:16:09 +0800

    feat(biz,data/log):log

--- internal/data/book.go
+++ internal/data/book.go
@@ -4,13 +4,7 @@ import (
 	"context"
 	"lin-cms-go/internal/data/ent"
 	"lin-cms-go/internal/data/ent/book"
-	"lin-cms-go/pkg/core"
 )
-
-func GetBookAll(ctx context.Context, offset int, size int) (model []*ent.Book, err error) {
-	model, err = GetDB().Book.Query().Limit(size).Offset(offset).All(ctx)
-	return
-}
 
 func GetBookById(ctx context.Context, id int) (model *ent.Book, err error) {
 	model, err = GetDB().Book.Query().Where(book.ID(id)).First(ctx)
@@ -39,23 +33,9 @@ func DeleteBook(ctx context.Context, id int) (err error) {
 
 func GetBookCount(ctx context.Context) (count int, err error) {
 	count, err = GetDB().Book.Query().Count(ctx)
-
 	return
 }
 
-type Paging struct {
-	Page   int
-	Size   int
-	Offset int
-}
-
-func NewPaging(page, pageSize int) *Paging {
-	return &Paging{
-		Page:   page,
-		Size:   pageSize,
-		Offset: core.GetPageOffset(page, pageSize),
-	}
-}
 func (p *Paging) ListBook(ctx context.Context) (model []*ent.Book, err error) {
 	model, err = GetDB().Book.Query().Limit(p.Size).Offset(p.Offset).All(ctx)
 	return
