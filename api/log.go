2561e8be83c050676e5696bd2b6a34c330c0e413
LiYuanits <luo_book@163.com>
2021-10-21 16:20:13 +0800

    feat(biz,data/log):log

--- api/log.go
+++ api/log.go
@@ -2,29 +2,26 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"lin-cms-go/internal/request"
 	"lin-cms-go/internal/biz"
+	"lin-cms-go/internal/request"
 	"lin-cms-go/pkg/core"
 )
 
 func Upload(c *fiber.Ctx) error {
-
 	return core.SuccessResp(c)
 }
 func GetLogs(c *fiber.Ctx) error {
 	var req request.GetLogs
 	if err := core.ParseRequest(c, &req); err != nil {
-	
 		return err
 	}
-
-	data, err := biz.GetLogs(req)
+	data, total, err := biz.GetLogs(c.Context(), req)
 	if err != nil {
 		return err
 	}
-	return core.SetData(c, data)
-
+	return core.SetPage(c, data, total)
 }
+
 func SearchLogs(c *fiber.Ctx) error {
 	var req request.SearchLogs
 	if err := core.ParseRequest(c, &req); err != nil {
@@ -43,11 +40,9 @@ func GetLogUsers(c *fiber.Ctx) error {
 	if err := core.ParseRequest(c, &req); err != nil {
 		return err
 	}
-
-	data, err := biz.GetLogUsers(req)
+	data, total, err := biz.GetLogUsers(c.Context(), req)
 	if err != nil {
 		return err
 	}
-
-	return core.SetData(c, data)
+	return core.SetPage(c, data, total)
 }
