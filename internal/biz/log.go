a301d03216a38d34179c560b311ed506a4169fcc
LiYuanits <luo_book@163.com>
2021-10-25 10:54:02 +0800

    refactor:log

--- internal/biz/log.go
+++ internal/biz/log.go
@@ -11,24 +11,23 @@ import (
 	"time"
 )
 
-func GetLogs(ctx context.Context, req request.GetLogs) (res interface{}, total int, err error) {
+func GetLogs(ctx context.Context, req request.GetLogs, page int, size int) (res interface{}, total int, err error) {
 	var logs []*model.LinLog
-	paging := data.NewPaging(req.Page, req.Count)
-
-	if req.Name == "" && req.Start == "" && req.End == "" {
-		logs, err = paging.GetLogAll(ctx)
-		total = data.GetLosTotal(ctx)
+	var start time.Time
+	var end time.Time
+	paging := data.NewPaging(page, size)
+	var query []predicate.LinLog
+	if req.Name != "" {
+		query = append(query, data.WithUsername(req.Name))
 	}
-	if req.Name != "" && req.Start == "" && req.End == "" {
-		logs, err = paging.FindLogsByUsername(ctx, req.Name)
-		total = data.GetLogsByUsernameTotal(ctx, req.Name)
+	if req.Start != "" && req.End != "" {
+		start = utils.String2time(req.Start)
+		end = utils.String2time(req.End)
+		q := linlog.And(linlog.CreateTimeGT(start), linlog.CreateTimeLT(end))
+		query = append(query, q)
 	}
-	if req.Name != "" && req.Start != "" && req.End != "" {
-		start := utils.String2time(req.Start)
-		end := utils.String2time(req.End)
-		logs, err = paging.FindLogsByUsernameAndRange(ctx, req.Name, start, end)
-		total = data.GetLogsByUsernameAndRangeTotal(ctx, req.Name, start, end)
-	}
+	logs, err = paging.Search(ctx, query)
+	total = data.GetSearchTotal(ctx, query)
 	if err != nil {
 		return
 	}
@@ -36,12 +35,11 @@ func GetLogs(ctx context.Context, req request.GetLogs) (res interface{}, total i
 	return
 }
 
-func SearchLogs(ctx context.Context, req request.SearchLogs) (res interface{}, total int, err error) {
+func SearchLogs(ctx context.Context, req request.SearchLogs, page int, size int) (res interface{}, total int, err error) {
 	var logs []*model.LinLog
 	var start time.Time
 	var end time.Time
-	paging := data.NewPaging(req.Page, req.Count)
-
+	paging := data.NewPaging(page, size)
 	var query []predicate.LinLog
 	if req.Name != "" {
 		query = append(query, data.WithUsername(req.Name))
@@ -56,39 +54,7 @@ func SearchLogs(ctx context.Context, req request.SearchLogs) (res interface{}, t
 		query = append(query, data.WithKeyword(req.Name))
 	}
 	logs, err = paging.Search(ctx, query)
-
-	//if req.Name != "" && req.Start != "" && req.End != "" && req.Keyword != "" {
-	//	logs, err = paging.FindLogsByUsernameAndRangeAndKeyword(ctx, req.Name, req.Keyword, start, end)
-	//	total = data.GetLogsByUsernameAndRangeAndKeywordTotal(ctx, req.Name, req.Keyword, start, end)
-	//}
-	//if req.Name == "" && req.Start == "" && req.End == "" && req.Keyword == "" {
-	//	logs, err = paging.GetLogAll(ctx)
-	//	total = data.GetLosTotal(ctx)
-	//}
-	//if req.Keyword != "" && req.Start != "" && req.End != "" && req.Name == "" {
-	//	logs, err = paging.FindLogsByRangeAndKeyword(ctx, req.Keyword, start, end)
-	//	total = data.GetLogsByRangeAndKeywordTotal(ctx, req.Keyword, start, end)
-	//}
-	//if req.Keyword != "" && req.Name != "" && req.Start == "" && req.End == "" {
-	//	logs, err = paging.FindLogsByUsernameAndKeyword(ctx, req.Name, req.Keyword)
-	//	total = data.GetLogsByUsernameAndKeywordTotal(ctx, req.Name, req.Keyword)
-	//}
-	//if req.Keyword != "" && req.Name == "" && req.Start == "" && req.End == "" {
-	//	logs, err = paging.FindLogsByKeyword(ctx, req.Keyword)
-	//	total = data.GetLogsByKeywordTotal(ctx, req.Keyword)
-	//}
-	//if req.Keyword == "" && req.Name == "" && req.Start != "" && req.End != "" {
-	//	logs, err = paging.FindLogsByRange(ctx, start, end)
-	//	total = data.GetLogsByRangeTotal(ctx, start, end)
-	//}
-	//if req.Keyword == "" && req.Name != "" && req.Start == "" && req.End == "" {
-	//	logs, err = paging.FindLogsByUsername(ctx, req.Name)
-	//	total = data.GetLogsByUsernameTotal(ctx, req.Name)
-	//}
-	//if req.Keyword == "" && req.Name != "" && req.Start != "" && req.End != "" {
-	//	logs, err = paging.FindLogsByUsernameAndRange(ctx, req.Name, start, end)
-	//	total = data.GetLogsByUsernameAndRangeTotal(ctx, req.Name, start, end)
-	//}
+	total = data.GetSearchTotal(ctx, query)
 	res = logs
 	return
 }
