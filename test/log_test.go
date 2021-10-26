a2f9e93230fe6612c48b22714cac798e5b351972
xushuhui <474497097@qq.com>
2021-10-26 17:14:59 +0800

    docs, model

---
+++ test/log_test.go
@@ -0,0 +1,27 @@
+package test
+
+import (
+	"testing"
+
+
+)
+
+func TestGetLogUsers(t *testing.T) {
+	// type args struct {
+	// 	c *fiber.Ctx
+	// }
+	// tests := []struct {
+	// 	name    string
+	// 	args    args
+	// 	wantErr bool
+	// }{
+	// 	// TODO: Add test cases.
+	// }
+	// for _, tt := range tests {
+	// 	t.Run(tt.name, func(t *testing.T) {
+	// 		if err := api.GetLogUsers(tt.args.c); (err != nil) != tt.wantErr {
+	// 			t.Errorf("GetLogUsers() error = %v, wantErr %v", err, tt.wantErr)
+	// 		}
+	// 	})
+	// }
+}
