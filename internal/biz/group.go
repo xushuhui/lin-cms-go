b9fbf4c5cc63d8f001bf8e36aa31fb0cbee88609
LiYuanits <luo_book@163.com>
2021-11-08 16:54:35 +0800

    feat:group

--- internal/biz/group.go
+++ internal/biz/group.go
@@ -1,30 +1,104 @@
 package biz
 
 import (
+	"context"
+	"lin-cms-go/internal/data"
+	"lin-cms-go/internal/data/model"
 	"lin-cms-go/internal/request"
+	"lin-cms-go/pkg/core"
+	"lin-cms-go/pkg/enum"
+	"lin-cms-go/pkg/errcode"
 )
 
-func GetGroups() (data map[string]interface{}, err error) {
-	return
+type Group struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Info  string `json:"info"`
+	Level int8   `json:"level"`
 }
-func GetGroup(id int) (data map[string]interface{}, err error) {
-	//groupModel,e := model.GetLinGroupById(id)
-	//if e != nil {
-	//	return
-	//}
-	return
-}
-func CreateGroup(name, info string) (err error) {
-	//groupModel := model.Group{
-	//	Name: name,
-	//	Info:info,
-	//}
 
+func GetGroups(ctx context.Context) (res interface{}, err error) {
+	var linGroupModel []*model.LinGroup
+	linGroupModel, err = data.GetAllGroup(ctx)
+	res = simplifyGroup(linGroupModel)
 	return
 }
-func UpdateGroup(req request.UpdateGroup) (err error) {
+
+func GetGroup(ctx context.Context, id int) (res interface{}, err error) {
+	var linGroupModel *model.LinGroup
+	linGroupModel, err = data.GetLinGroupById(ctx, id, enum.ROOT)
+	if model.IsNotFound(err) {
+		err = core.NewErrorCode(errcode.GroupNotFound)
+		return
+	}
+	res = Group{Id: linGroupModel.ID, Name: linGroupModel.Name, Info: linGroupModel.Info, Level: linGroupModel.Level}
 	return
 }
-func DeleteGroup(id int) (err error) {
+
+func simplifyGroup(groupModel []*model.LinGroup) []Group {
+	res := make([]Group, 0, len(groupModel))
+	for _, v := range groupModel {
+		res = append(res, Group{Id: v.ID, Name: v.Name, Info: v.Info, Level: v.Level})
+	}
+	return res
+}
+
+func CreateGroup(ctx context.Context, name string, info string, permissionIds []int) (err error) {
+	group, _ := data.GetLinGroupByName(ctx, name)
+	if group != nil {
+		err = core.NewErrorCode(errcode.GroupFound)
+		return
+	}
+	for _, v := range permissionIds {
+		_, err = data.GetLinPermissionById(ctx, v)
+		if model.IsNotFound(err) {
+			err = core.NewErrorCode(errcode.PermissionNotFound)
+			return
+		}
+	}
+	groupModel, err := data.CreateGroup(ctx, name, info, enum.USER)
+	if err != nil {
+		return
+	}
+
+	err = data.BatchCreateGroupPermission(ctx, groupModel.ID, permissionIds)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func UpdateGroup(ctx context.Context, id int, req request.UpdateGroup) (err error) {
+	_, err = data.GetLinGroupById(ctx, id, enum.ROOT)
+	if model.IsNotFound(err) {
+		err = core.NewErrorCode(errcode.GroupNotFound)
+		return
+	}
+	err = data.UpdateGroup(ctx, id, req.Name, req.Info)
+	return
+}
+
+func DeleteGroup(ctx context.Context, id int) (err error) {
+	var linGroup *model.LinGroup
+	linGroup, err = data.GetLinGroupById(ctx, id, enum.ROOT)
+	if model.IsNotFound(err) {
+		err = core.NewErrorCode(errcode.GroupNotFound)
+		return
+	}
+	if err != nil {
+		return
+	}
+	if linGroup.Level == enum.ROOT {
+		err = core.NewErrorCode(errcode.RootGroupNotAllowDelete)
+	}
+
+	if linGroup.Level == enum.GUEST {
+		err = core.NewErrorCode(errcode.GuestGroupNotAllowDelete)
+	}
+
+	//err = data.DeleteGroupPermission(ctx, id)
+
+	// lin_group删除这个表的  lin_group_permission 删除这个表数据  lin_user_group 删除这个表的数据
+
 	return
 }
