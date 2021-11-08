6fca601e210a43339ab77d1b566d817d1ff24098
LiYuanits <luo_book@163.com>
2021-11-08 16:54:35 +0800

    feat:group

--- api/group.go
+++ api/group.go
@@ -9,55 +9,62 @@ import (
 )
 
 func GetGroups(c *fiber.Ctx) error {
-	data, err := biz.GetGroups()
+	data, err := biz.GetGroups(c.Context())
 	if err != nil {
-
 		return err
 	}
 	return core.SetData(c, data)
-
 }
+
 func GetGroup(c *fiber.Ctx) error {
 	id, err := utils.StringToInt(c.Params("id"))
 	if err != nil {
-
 		return err
 	}
-	data, err := biz.GetGroup(id)
+	data, err := biz.GetGroup(c.Context(), id)
 	if err != nil {
 		return err
 	}
 	return core.SetData(c, data)
-
 }
+
 func CreateGroup(c *fiber.Ctx) error {
 	var req request.CreateGroup
 	if err := core.ParseRequest(c, &req); err != nil {
 		return err
 	}
-
-	err := biz.CreateGroup(req.Name, req.Info)
-	return err
+	err := biz.CreateGroup(c.Context(), req.Name, req.Info, req.PermissionIds)
+	if err != nil {
+		return err
+	}
+	return core.SuccessResp(c)
 }
+
 func UpdateGroup(c *fiber.Ctx) error {
 	var req request.UpdateGroup
 	if err := core.ParseRequest(c, &req); err != nil {
 		return err
 	}
+	id, err := utils.StringToInt(c.Params("id"))
+	if err != nil {
+		return err
+	}
 
-	err := biz.UpdateGroup(req)
-
-	return err
+	err = biz.UpdateGroup(c.Context(), id, req)
+	if err != nil {
+		return err
+	}
+	return core.SuccessResp(c)
 }
 func DeleteGroup(c *fiber.Ctx) error {
 	id, err := utils.StringToInt(c.Params("id"))
 	if err != nil {
-
 		return err
 	}
-	err = biz.DeleteGroup(id)
-	if err != nil {
 
+	// TODO 删除分组功能还没写完
+	err = biz.DeleteGroup(c.Context(), id)
+	if err != nil {
 		return err
 	}
 	return core.SuccessResp(c)
--- pkg/errcode/message.go
+++ pkg/errcode/message.go
@@ -14,21 +14,32 @@ const (
 	UserNotFound
 	BookNotFound
 	BookTitleRepetition
+	GroupNotFound
+	GroupFound
+	RootGroupNotAllowDelete
+	GuestGroupNotAllowDelete
+
+	PermissionNotFound
 )
 
 var MsgFlags = map[int]string{
-	SUCCESS:               "ok",
-	InvalidParams:         "请求参数错误",
-	DBError:               "数据库错误",
-	ServerError:           "系统异常，请联系管理员！",
-	AuthCheckTokenFail:    "Token鉴权失败",
-	AuthCheckTokenTimeout: "Token已超时",
-	ErrorAuthToken:        "Token错误",
-	ErrorPassWord:         "密码错误",
-	UserFound:             "用户已存在",
-	UserNotFound:          "用户不存在",
-	BookNotFound:          "书籍不存在",
-	BookTitleRepetition:   "书籍标题重复",
+	SUCCESS:                  "ok",
+	InvalidParams:            "请求参数错误",
+	DBError:                  "数据库错误",
+	ServerError:              "系统异常，请联系管理员！",
+	AuthCheckTokenFail:       "Token鉴权失败",
+	AuthCheckTokenTimeout:    "Token已超时",
+	ErrorAuthToken:           "Token错误",
+	ErrorPassWord:            "密码错误",
+	UserFound:                "用户已存在",
+	UserNotFound:             "用户不存在",
+	BookNotFound:             "书籍不存在",
+	BookTitleRepetition:      "书籍标题重复",
+	GroupNotFound:            "分组不存在",
+	RootGroupNotAllowDelete:  "root分组不允许删除",
+	GuestGroupNotAllowDelete: "guest分组不允许删除",
+	GroupFound:               "分组已存在",
+	PermissionNotFound:       "权限不存在",
 }
 
 func GetMsg(code int) string {
